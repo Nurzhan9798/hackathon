14c2e1113c310eb74cded0a3b7603e4a2b94e60a
Nurzhan <bakytov9798@gmail.com>
2023-12-01 15:52:37 +0600

    links

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <div className="App">
       <Link to={"/"}>Home</Link>
-      <Link to={"/"}>EventList</Link>
-      <Link to={"/"}>EventOverview</Link>
-      <Link to={"/"}>Map</Link>
-      <Link to={"/places/:id"}>Place overview</Link>
+      <Link to={"/events"}>EventList</Link>
+      <Link to={"/events/5"}>EventOverview</Link>
+      <Link to={"/map"}>Map</Link>
+      <Link to={"/places/3"}>Place overview</Link>
       <Link to={"/info"}>Home</Link>
       <Routes>
         <Route path={"/"} element={<h1>Home</h1>} />
@@ -24,45 +24,6 @@ function App() {
         <Route path={"/places/:id"} element={<h1>Place overview</h1>} />
         <Route path={"/info"} element={<h1>Info</h1>} />
       </Routes>
-
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-
-      {/*<MapContainer className={"map"} center={[43.693695, 51.240834]} zoom={13} scrollWheelZoom={false}>*/}
-      {/*    <TileLayer*/}
-      {/*        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'*/}
-      {/*        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"*/}
-      {/*    />*/}
-      {/*    <Marker position={[51.505, -0.09]}>*/}
-      {/*        <Popup>*/}
-      {/*            A pretty CSS3 popup. <br /> Easily customizable.*/}
-      {/*        </Popup>*/}
-      {/*    </Marker>*/}
-      {/*</MapContainer>*/}
     </div>
   );
 }
